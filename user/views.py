0d326d3cb2e1214da3e070588a0229d27f383695
Никита <pyp18@yandex.ru>
2021-06-06 16:55:46 +0300

    Blessed one

--- user/views.py
+++ user/views.py
@@ -6,11 +6,11 @@ from recipes.forms import SignUpForm
 
 def signup(request):
     form = SignUpForm(request.POST, files=request.FILES or None)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        raw_password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=raw_password)
-        login(request, user)
-        return redirect('index')
-    return render(request, 'registration/signup.html', {'form': form})
+    if not form.is_valid():
+        return render(request, 'registration/signup.html', {'form': form})
+    form.save()
+    username = form.cleaned_data.get('username')
+    raw_password = form.cleaned_data.get('password1')
+    user = authenticate(username=username, password=raw_password)
+    login(request, user)
+    return redirect('index')
