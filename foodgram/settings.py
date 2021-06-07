ad5b48e4c4cecd955dd2ce41b5017429a98f30f9
pyp18 <pyp18@yandex.ru>
2021-06-07 13:42:49 +0300

    Working Docker

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'MyDataBase',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
