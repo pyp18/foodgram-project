0f24f36d430518d71c56780d1d7bd83524f1dcd4
pyp18 <pyp18@yandex.ru>
2021-06-11 17:14:04 +0300

    signup fix

--- user/views.py
+++ user/views.py
@@ -4,13 +4,17 @@ from django.shortcuts import redirect
 from recipes.forms import SignUpForm
 
 
-def signup(request):
-    form = SignUpForm(request.POST, files=request.FILES or None)
-    if not form.is_valid():
-        return render(request, 'registration/signup.html', {'form': form})
-    form.save()
-    username = form.cleaned_data.get('username')
-    raw_password = form.cleaned_data.get('password1')
-    user = authenticate(username=username, password=raw_password)
-    login(request, user)
-    return redirect('index')
+def signup(request): 
+    if request.method == 'POST': 
+        form = SignUpForm(request.POST) 
+        if form.is_valid(): 
+            form.save() 
+            username = form.cleaned_data.get('username') 
+            raw_password = form.cleaned_data.get('password1') 
+            user = authenticate(username=username, password=raw_password) 
+            login(request, user) 
+            return redirect('index') 
+    form = SignUpForm(request.POST) 
+    print(form) 
+    form = SignUpForm() 
+    return render(request, 'signup.html', {'form': form}) 
