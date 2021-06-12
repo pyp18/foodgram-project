b907639392d28c6c7c04ecfabae8b9e707539cd1
pyp18 <pyp18@yandex.ru>
2021-06-12 13:00:17 +0300

    try

--- recipes/forms.py
+++ recipes/forms.py
@@ -46,12 +46,3 @@ class IngredientsForm(ModelForm):
         labels = {'title': 'Название', 'unit': 'Единицы  измерения'}
 
 
-class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(
-        max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(
-        max_length=254, help_text='Required. Inform a valid email address.')
-
-    class Meta:
-        model = User
-        fields = ('first_name', 'username', 'email', 'password1', 'password2')
--- user/views.py
+++ user/views.py
@@ -1,34 +1,12 @@
-from django.shortcuts import render
 from django.contrib.auth import login, authenticate
-from django.shortcuts import redirect
-from recipes.forms import SignUpForm
-
-from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
-def signup(request): 
-
-    if request.method == 'POST': 
-        form = SignUpForm(request.POST) 
-        if form.is_valid(): 
-            form.save() 
-            username = form.cleaned_data.get('username') 
-            raw_password = form.cleaned_data.get('password1') 
-            user = authenticate(username=username, password=raw_password) 
-            login(request, user) 
-            return redirect('index') 
-    else: 
-        form = SignUpForm(request.POST) 
-
-        print(form) 
-    return render(request, 'registration/signup.html', {'form': form}) 
-
 
 
 def signup2(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -37,5 +15,5 @@ def signup2(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/signup2.html', {'form': form})
