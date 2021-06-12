7022c689296e57d40a1c2ed23dd3d81f567bfb69
pyp18 <pyp18@yandex.ru>
2021-06-12 16:28:29 +0300

    tut

--- user/views.py
+++ user/views.py
@@ -13,7 +13,7 @@ def signup2(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('index')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
