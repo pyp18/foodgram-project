506c6a25ab4f3fa81d6d35847a2d1caf5322b4cc
pyp18 <pyp18@yandex.ru>
2021-06-22 22:22:28 +0300

    wtf

--- user/forms.py
+++ user/forms.py
@@ -3,12 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30, required=True, help_text='Optional', error_messages={'required': 'Никнейм обязателен'})
-    first_name = forms.CharField(max_length=30, required=True, help_text='Optional', error_messages={'required': 'Имя обязательно'})
-    email = forms.EmailField(max_length=254, help_text='Enter a valid email address', error_messages={'required': 'Почта должна быть корректной'})
-    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput, error_messages={'required': 'Почта должна быть корректной'})
-    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput, error_messages={'required': 'Почта должна быть корректной'},
-        help_text=_("Enter the same password as above, for verification."))
+    first_name = forms.CharField(max_length=30, required=True, help_text='Optional', error_messages={'required': 'firstname required'})
+    email = forms.EmailField(max_length=254, help_text='Enter a valid email address', error_messages={'required': 'email should be correct'})
 
     class Meta:
         model = User
