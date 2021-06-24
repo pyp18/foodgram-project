4d60b2834adc2c9c0afbadb6420943ae3935df99
pyp18 <pyp18@yandex.ru>
2021-06-24 21:41:38 +0300

    wowo

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -135,10 +135,10 @@ GLOBALPAGINATOR = 6
 
 
 
-EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'aonda1970nikita18@gmail.com'
-EMAIL_HOST_PASSWORD = 'dppkfzpstbeqrxqu'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'aonda1970nikita18@gmail.com'
+EMAIL_HOST_PASSWORD = 'dppkfzpstbeqrxqu'
 DEFAULT_FROM_EMAIL = 'Foodgramm'
--- foodgram/urls.py
+++ foodgram/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password')
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),    
 ]
 
 
