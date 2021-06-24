2edd787dd1576ec549119133782db0ef856e84ad
pyp18 <pyp18@yandex.ru>
2021-06-24 20:52:51 +0300

    wewо

--- test.py
+++ test.py
@@ -3,4 +3,4 @@
 
 28200cd95fa7   nginx:1.19.3                        "/docker-entrypoint.…"   9 minutes ago   Up 9 minutes   0.0.0.0:80->80/tcp, :::80->80/tcp   pyp18_nginx_1
 416822bcb537   pyp18/foodgram-project_web:latest   "/bin/sh -c 'gunicor…"   9 minutes ago   Up 9 minutes                                       pyp18_web_1
-54f559a46bd2 5555
+54f559a46bd2 55558
