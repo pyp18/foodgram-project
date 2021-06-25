661993a2a771d271c4ec7f2ddc9ad0d71f5ac584
pyp18 <pyp18@yandex.ru>
2021-06-25 13:34:44 +0300

    Done

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/set_new_password.html'), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_completed'), name='password_reset_complete'),    
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_completed.html'), name='password_reset_complete'),    
 ]
 
 
