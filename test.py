322efbffaaf66a63a8a944b072f8a1913891ea25
pyp18 <pyp18@yandex.ru>
2021-06-25 11:52:24 +0300

    wowo

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-156776
+<h1>Forgot your password?</h1>
+<p>Enter your e-mail address to obtain a new password.</p>
 
-
-28200cd95fa7   nginx:1.19.3                        "/docker-entrypoint.…"   9 minutes ago   Up 9 minutes   0.0.0.0:80->80/tcp, :::80->80/tcp   pyp18_nginx_1
-416822bcb537   pyp18/foodgram-project_web:latest   "/bin/sh -c 'gunicor…"   9 minutes ago   Up 9 minutes                                       pyp18_web_1
-54f559a46bd2 55558
+<form action="." method="post">
+    {% csrf_token %}
+    {{ form.as_p }}
+    <input type="submit" value="Send me instructions!">
+</form>
