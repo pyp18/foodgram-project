3c7153115d34f378cae9e54bd29f2363a3433ccf
pyp18 <pyp18@yandex.ru>
2021-06-27 14:04:12 +0300

    True

--- recipes/models.py
+++ recipes/models.py
@@ -130,7 +130,8 @@ class Follow(models.Model):
                 name='unique_author'
             )
         ]
-
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
 
     def __str__(self):
         return f'{self.user} на {self.author}'
